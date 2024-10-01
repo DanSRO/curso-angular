47204e927014b4a7d2b94198045dcae968e86be4
daniel dantas <Daniel Dantas>
2024-10-01 00:24:49 -0300

    Created service with remove

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { Animal } from '../../Animal';
+import { ListService } from '../../services/list.service';
 
 @Component({
   selector: 'app-list-render',
@@ -18,8 +19,12 @@ export class ListRenderComponent {
   ]
   
   animalsDetail:string = '';
-  
+  listService: ListService = new ListService;
   showAge(animal: Animal): void{
     this.animalsDetail = `O pet ${animal.name} tem ${animal.age} anos de idade.`;
+  }
+  removeAnimal(animal:Animal){
+    console.log("Removendo animal...");
+    this.animals = this.listService.remove(this.animals, animal);
   }
 }
